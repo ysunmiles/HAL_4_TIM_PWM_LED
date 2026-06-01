f2c12d9d7324944aa8af2e9640b0a8099ccd6d1e
ysunmiles <ysunmiles@gmail.com>
2026-06-01 18:12:15 +0200

    优化程序

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -107,7 +107,7 @@ int main(void)
       HAL_Delay(30);
     }
 
-    for (uint16_t pwm_value = 99; pwm_value >= 0; pwm_value -= 1)
+    for (uint16_t pwm_value = 99; pwm_value > 0; pwm_value -= 1)
     {
       __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwm_value);
       OLED_ShowNum(2, 3, pwm_value, 3);
